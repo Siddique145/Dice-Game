da6786a085d24f428efe1993e3da55ec953ac465
Siddique145 <muhammadsiddique14344@gmail.com>
2024-04-29 13:53:25 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+
+var user_num = document.getElementById('user_num')
+var result_num = document.getElementById('result_num')
+var result_box = document.getElementById('result_box')
+
+function checkNumber(userNumber) {
+
+    var randomNumber = Math.ceil(Math.random() * 6)
+    user_num.innerText = userNumber
+    result_num.innerText = randomNumber
+    if (userNumber == randomNumber) {
+        result_box.innerText = 'Congrats!, You won'
+        result_box.style.color = 'orange'
+    } else {
+        result_box.innerText = 'Soory!, You lose , Try Again'
+        result_box.style.color = 'red'
+    }
+}
